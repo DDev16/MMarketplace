84db74bc169ac98a9ba6a63447e4ff7ea76fa375
DDev16 <dillondevera12@gmail.com>
2023-01-03 19:02:33 -0700

    first commit

---
+++ engine/configuration.js
@@ -0,0 +1,80 @@
+import { NFTStorage } from "nft.storage";
+
+export const client = new NFTStorage({
+  token: process.env.NEXT_PUBLIC_NFTSTORAGE_API_KEY,
+});
+
+/*
+Songbird Testnet
+*/
+export var hhresell = "0x02d6480848a386384A00FDE469D5C10926E51C67";
+export var hhnftcol = "0x7B6fF7a51a62f6F9dAc586aF1011A973288dEF61";
+export var hhnft = "0xD5d496aa89f94D06c8605aDCd38eD3CA896Df6Fa";
+export var hhmarket = "0xa6BFbefB1FC536b0825eD11996CF148C24054582";
+export var hhrpc = "https://songbird.towolabs.com/rpc";
+
+/*
+Flare Mainnet
+*/ 
+export var flrresell = "0x02d6480848a386384A00FDE469D5C10926E51C67";
+export var flrnftcol = "0x7B6fF7a51a62f6F9dAc586aF1011A973288dEF61";
+export var flrnft = "0xD5d496aa89f94D06c8605aDCd38eD3CA896Df6Fa";
+export var flrmarket = "0xa6BFbefB1FC536b0825eD11996CF148C24054582";
+export var flrrpc = "https://songbird.towolabs.com/rpc";
+
+
+/*
+Eth Mainnet
+*/
+export var ethresell = "0xD6bEa3f2f34a44DA703f3514b3Ce7aA7EdD19fa0";
+export var ethnftcol = "0xC6e16c3ce24120B70f3967D1b8ad364577F70AAf";
+export var ethnft = "0x6FfF137677FEBee150628E767E926444218aBF9a";
+export var ethmarket = "0x56A7BE395206d86520a326E761DE551A827013DA";
+export var ethrpc = "https://rpc.ankr.com/eth_goerli";
+
+/*
+Binance Mainnet
+*/
+export var bnbresell = "0x7aBFb92BBF795B22796B2C86709b4b40E9E90e95";
+export var bnbnftcol = "0x7634E8176bd3D6156dB154bf4df237ca184b6C9f";
+export var bnbnft = "0x60e6959a8652996D835055813A8b8c02FCa3E2E0";
+export var bnbmarket = "0xfC5583Bb036698cCda08199a4c8D291d76fAbffB";
+export var bnbrpc = "https://data-seed-prebsc-1-s3.binance.org:8545/";
+
+
+/*
+Polygon Mainnet
+*/
+export var polyresell = "0x935feEaA1e4959C7DeB9C3AF0B3B75C2b7e09d70";
+export var polynftcol = "0x65f4643394437E0D27a151AB6e0ebbd72019089F";
+export var polynft = "0x3dea30E978E8D042FdA3FF468665b0fd36720fBb";
+export var polymarket = "0x2C35b5366011fa9B3531fef52d8D21c575153c0A";
+export var polyrpc = "https://matic-mumbai.chainstacklabs.com";
+
+
+/*
+Goerli Testnet
+*/
+export var goeresell = "0xD6bEa3f2f34a44DA703f3514b3Ce7aA7EdD19fa0";
+export var goenftcol = "0xC6e16c3ce24120B70f3967D1b8ad364577F70AAf";
+export var goenft = "0x6FfF137677FEBee150628E767E926444218aBF9a";
+export var goemarket = "0x56A7BE395206d86520a326E761DE551A827013DA";
+export var goerpc = "https://rpc.ankr.com/eth_goerli";
+
+/*
+BSC Testnet
+*/
+export var bsctresell = "0x7aBFb92BBF795B22796B2C86709b4b40E9E90e95";
+export var bsctnftcol = "0x7634E8176bd3D6156dB154bf4df237ca184b6C9f";
+export var bsctnft = "0x60e6959a8652996D835055813A8b8c02FCa3E2E0";
+export var bsctmarket = "0xfC5583Bb036698cCda08199a4c8D291d76fAbffB";
+export var bsctrpc = "https://data-seed-prebsc-1-s3.binance.org:8545/";
+
+/*
+Mumbai Testnet
+*/
+export var mmresell = "0x935feEaA1e4959C7DeB9C3AF0B3B75C2b7e09d70";
+export var mmnftcol = "0x65f4643394437E0D27a151AB6e0ebbd72019089F";
+export var mmnft = "0x3dea30E978E8D042FdA3FF468665b0fd36720fBb";
+export var mmmarket = "0x2C35b5366011fa9B3531fef52d8D21c575153c0A";
+export var mmrpc = "https://matic-mumbai.chainstacklabs.com";
